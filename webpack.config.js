f6aa75c550674bda6ac20f118ad941040c99edd4
aron <aron@olioapps.com>
2018-08-28 19:21:04 -0700

    draft

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require("webpack");
+const path = require("path");
+const DashboardPlugin = require("webpack-dashboard/plugin");
+const nodeEnv = process.env.NODE_ENV || "development";
+const isProd = nodeEnv === "production";
+
+const plugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      // eslint-disable-line quote-props
+      NODE_ENV: JSON.stringify(nodeEnv)
+    }
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: "vendor",
+    minChunks: Infinity,
+    filename: "vendor.bundle.js"
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      tslint: {
+        emitErrors: true,
+        failOnHint: true
+      }
+    }
+  })
+];
+
+if (!isProd) {
+  plugins.push(new DashboardPlugin());
+}
+
+var config = {
+  devtool: isProd ? "hidden-source-map" : "source-map",
+  context: path.resolve("./src"),
+  entry: {
+    app: "./index.ts",
+    vendor: "./vendor.ts"
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].bundle.js",
+    sourceMapFilename: "[name].bundle.map",
+    devtoolModuleFilenameTemplate: function(info) {
+      return "file:///" + info.absoluteResourcePath;
+    }
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.ts?$/,
+        exclude: ["node_modules"],
+        use: ["awesome-typescript-loader", "source-map-loader"]
+      },
+      {
+        test: /\.(js|ts)$/,
+        loader: "babel-loader",
+        exclude: [/\/node_modules\//]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: plugins
+};
+
+module.exports = config;
