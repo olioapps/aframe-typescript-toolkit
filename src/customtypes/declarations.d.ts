ed587c92b4718c8b74530f4a179c73622fa9ae61
aron <aron@olioapps.com>
2018-09-17 21:06:01 -0700

    cleaning up

--- src/customtypes/declarations.d.ts
+++ src/customtypes/declarations.d.ts
@@ -1,15 +1,3 @@
-declare module "aframe-extras" {
-    const value: any;
-    export default value;
-}
-
-declare module "redux-saga" {
-    const value: any
-    export default value
-	export const takeEvery: any
-	export const delay: any
-}
-
 declare module "aframe" {
     const value: any;
     export default value;
