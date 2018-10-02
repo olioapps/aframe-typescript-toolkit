1f61caecb9c5f458c58b7a842500cd730438f17c
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2018-10-01 17:28:17 -0700

    inline docs for component wrapper

--- dist/index.js
+++ dist/index.js
@@ -51,7 +51,35 @@ var getInstanceMethodNames = function (obj, stop) {
  * Extend this class to create strongly typed A-Frame components.
  * Default implementations for component lifecycle methods such as init(), tick(), and others are provided,
  * and can be overridden for your component's specific behavior.
- */
+ * Additional Information on A-Frame components can be found here: [A-Frame Docs: Component](https://aframe.io/docs/0.8.0/core/component.html)
+ *
+ *
+* @example
+* ```typescript
+*
+* interface NewComponentSchema {
+*    readonly name: string
+*    readonly value: number
+*}
+*
+* export class NewComponent extends ComponentWrapper<NewComponentSchema> {
+*     constructor() {
+*        super("new-component", {
+*            name: {
+*                default: "new",
+*            },
+*            value: {
+*                default: 0,
+*            },
+*        })
+*    }
+*
+*   tick() {
+*       ...
+*   }
+*}
+* ```
+*/
 var ComponentWrapper = /** @class */function () {
     function ComponentWrapper(name, schema) {
         this.name = name;
@@ -60,30 +88,30 @@ var ComponentWrapper = /** @class */function () {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // default aframe core function implementations
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_remove.
+     * Wraps A-Frame lifecycle handler method: [remove](https://aframe.io/docs/0.8.0/core/component.html#remove). See A-Frame documentation for more details.
      */
     ComponentWrapper.prototype.remove = function () {};
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_update.
+    * Wraps A-Frame lifecycle handler method: [update](https://aframe.io/docs/0.8.0/core/component.html#update-olddata). See A-Frame documentation for more details.
      */
     ComponentWrapper.prototype.update = function (oldData) {};
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_updateschema.
+      * Wraps A-Frame lifecycle handler method: [updateschema](https://aframe.io/docs/0.8.0/core/component.html#updateschema-data). See A-Frame documentation for more details.
      */
     ComponentWrapper.prototype.extendSchema = function (update) {};
     ComponentWrapper.prototype.flushToDOM = function () {};
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // default aframe core function implementations
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_init.
+     * Wraps A-Frame lifecycle handler method: [init](https://aframe.io/docs/0.8.0/core/component.html#init).
      */
     ComponentWrapper.prototype.init = function () {};
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_pause.
+     * Wraps A-Frame lifecycle handler method: [pause](https://aframe.io/docs/0.8.0/core/component.html#pause).
      */
     ComponentWrapper.prototype.pause = function () {};
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_play.
+     * Wraps A-Frame lifecycle handler method: [play](https://aframe.io/docs/0.8.0/core/component.html#play).
      */
     ComponentWrapper.prototype.play = function () {};
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -100,6 +128,9 @@ var ComponentWrapper = /** @class */function () {
     };
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // special wrapper functions implementations
+    /**
+    * Call this method to remove/detach A-Frame entity from scene
+    */
     ComponentWrapper.prototype.destroy = function () {
         var parent = this.el.parentElement;
         if (!!parent) {
@@ -117,6 +148,27 @@ var ComponentWrapper = /** @class */function () {
     return ComponentWrapper;
 }();
 exports.ComponentWrapper = ComponentWrapper;
+/**
+ * Extend this class to create strongly typed A-Frame System.
+ * Like components, there are default implementations for component lifecycle
+ * methods such as init(), play(), pause(), and tick()
+ * that can be overridden.
+ * Additional Information on A-Frame systems can be found here: [A-Frame Docs: System](https://aframe.io/docs/0.8.0/core/systems.html)
+ *
+ *
+* @example
+* ```typescript
+*
+*export class NewSystem extends SystemWrapper {
+*    constructor() {
+*        super("new")
+*
+*        new NewComponent().register()
+*    }
+*}
+*
+* ```
+*/
 var SystemWrapper = /** @class */function () {
     function SystemWrapper(name, schema) {
         this.name = name;
--- dist/src/aframe_wrapper.d.ts
+++ dist/src/aframe_wrapper.d.ts
@@ -3,7 +3,35 @@
  * Extend this class to create strongly typed A-Frame components.
  * Default implementations for component lifecycle methods such as init(), tick(), and others are provided,
  * and can be overridden for your component's specific behavior.
- */
+ * Additional Information on A-Frame components can be found here: [A-Frame Docs: Component](https://aframe.io/docs/0.8.0/core/component.html)
+ *
+ *
+* @example
+* ```typescript
+*
+* interface NewComponentSchema {
+*    readonly name: string
+*    readonly value: number
+*}
+*
+* export class NewComponent extends ComponentWrapper<NewComponentSchema> {
+*     constructor() {
+*        super("new-component", {
+*            name: {
+*                default: "new",
+*            },
+*            value: {
+*                default: 0,
+*            },
+*        })
+*    }
+*
+*   tick() {
+*       ...
+*   }
+*}
+* ```
+*/
 export declare abstract class ComponentWrapper<SCHEMA = {}, SYSTEM extends AFrame.System = AFrame.System> implements AFrame.Component<SCHEMA, SYSTEM> {
     el: AFrame.Entity;
     id: string;
@@ -13,38 +41,62 @@ export declare abstract class ComponentWrapper<SCHEMA = {}, SYSTEM extends AFram
     name: string;
     constructor(name: string, schema?: AFrame.Schema<SCHEMA>);
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_remove.
+     * Wraps A-Frame lifecycle handler method: [remove](https://aframe.io/docs/0.8.0/core/component.html#remove). See A-Frame documentation for more details.
      */
     remove(): void;
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_update.
+    * Wraps A-Frame lifecycle handler method: [update](https://aframe.io/docs/0.8.0/core/component.html#update-olddata). See A-Frame documentation for more details.
      */
     update(oldData: {}): void;
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_updateschema.
+      * Wraps A-Frame lifecycle handler method: [updateschema](https://aframe.io/docs/0.8.0/core/component.html#updateschema-data). See A-Frame documentation for more details.
      */
     extendSchema(update: {}): void;
     flushToDOM(): void;
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_init.
+     * Wraps A-Frame lifecycle handler method: [init](https://aframe.io/docs/0.8.0/core/component.html#init).
      */
     init(): void;
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_pause.
+     * Wraps A-Frame lifecycle handler method: [pause](https://aframe.io/docs/0.8.0/core/component.html#pause).
      */
     pause(): void;
     /**
-     * Wraps https://aframe.io/docs/0.8.0/core/component.html#definition_lifecycle_handler_methods_play.
+     * Wraps A-Frame lifecycle handler method: [play](https://aframe.io/docs/0.8.0/core/component.html#play).
      */
     play(): void;
     /***
      * @hidden
      */
     merge(): void;
+    /**
+    * Call this method to remove/detach A-Frame entity from scene
+    */
     destroy(): void;
     register(): this;
     registerCallback(callbackName: string, fn: Function): void;
 }
+/**
+ * Extend this class to create strongly typed A-Frame System.
+ * Like components, there are default implementations for component lifecycle
+ * methods such as init(), play(), pause(), and tick()
+ * that can be overridden.
+ * Additional Information on A-Frame systems can be found here: [A-Frame Docs: System](https://aframe.io/docs/0.8.0/core/systems.html)
+ *
+ *
+* @example
+* ```typescript
+*
+*export class NewSystem extends SystemWrapper {
+*    constructor() {
+*        super("new")
+*
+*        new NewComponent().register()
+*    }
+*}
+*
+* ```
+*/
 export declare abstract class SystemWrapper<SCHEMA = {}> implements AFrame.System {
     data: SCHEMA;
     schema: AFrame.Schema<SCHEMA>;
